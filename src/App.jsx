73d0d594f99dd2474914ef15854639d9079c8674
Rogelio Martinez <rogejr89@gmail.com>
2024-09-25 20:50:55 -0400

    stopping at adding time travel. the main tic tac toe game is done.

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   
   function handleClick(inx) {
-    if (squares[inx]) {
+    if (squares[inx] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -21,8 +21,17 @@ function App() {
     setXIsNext(!xIsNext);
   }
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <SquareButton value={squares[0]} onSquareClick={() => handleClick(0)}/>
         <SquareButton value={squares[1]} onSquareClick={() => handleClick(1)}/>
@@ -40,6 +49,26 @@ function App() {
       </div>
     </>
   )
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default App
