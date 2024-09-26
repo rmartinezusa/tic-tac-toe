34fdd9ebe97d1f45acac2552cd25d01b73ad0fcd
Rogelio Martinez <rogejr89@gmail.com>
2024-09-25 20:41:28 -0400

    deleted file components SquareButtonRow.jsx it was difficult to follow tutorial.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,43 @@
 import { useState } from 'react'
-import SquareButtonRow from './SquareButtonRow'
+import SquareButton from './SquareButton'
 
 function App() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  
+  function handleClick(inx) {
+    if (squares[inx]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+
+    if(xIsNext){
+      nextSquares[inx] = "X";
+    } else {
+      nextSquares[inx] = "O";
+    }
+
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
 
   return (
     <>
-    
-      <SquareButtonRow/>
-      <SquareButtonRow/>
-      <SquareButtonRow/>
+      <div className="board-row">
+        <SquareButton value={squares[0]} onSquareClick={() => handleClick(0)}/>
+        <SquareButton value={squares[1]} onSquareClick={() => handleClick(1)}/>
+        <SquareButton value={squares[2]} onSquareClick={() => handleClick(2)}/>
+      </div>
+      <div className="board-row">
+        <SquareButton value={squares[3]} onSquareClick={() => handleClick(3)}/>
+        <SquareButton value={squares[4]} onSquareClick={() => handleClick(4)}/>
+        <SquareButton value={squares[5]} onSquareClick={() => handleClick(5)}/>
+      </div>
+      <div className="board-row">
+        <SquareButton value={squares[6]} onSquareClick={() => handleClick(6)}/>
+        <SquareButton value={squares[7]} onSquareClick={() => handleClick(7)}/>
+        <SquareButton value={squares[8]} onSquareClick={() => handleClick(8)}/>
+      </div>
     </>
   )
 }
--- src/SquareButton.jsx
+++ src/SquareButton.jsx
@@ -1,14 +1,4 @@
-import { useState } from 'react'
-
-function SquareButton({}){
-
-    const [value, setValue] = useState(null);
-    
-    function handleClick() {
-        setValue("X")
-        console.log(">>>XXX<<<");
-    }
-
-    return (<button className="square" onClick={handleClick}>{value}</button>)
+function SquareButton({value, onSquareClick}){
+    return (<button className="square" onClick={onSquareClick}>{value}</button>)
 } 
 export default SquareButton
